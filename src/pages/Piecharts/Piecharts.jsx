f70a766d829a30afa4243695e4afacd362d09b61
Mohsen9april <Mohsen9april@gmail.com>
2023-01-17 00:10:53 +0330

    add piechart

--- src/pages/Piecharts/Piecharts.jsx
+++ src/pages/Piecharts/Piecharts.jsx
@@ -1,5 +1,23 @@
 import React from "react";
-import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Legend, Cell, ResponsiveContainer, Tooltip } from "recharts";
+
+const data01 = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+  { name: "Group E", value: 278 },
+  { name: "Group F", value: 189 },
+];
+
+const data02 = [
+  { name: "Group A", value: 2400 },
+  { name: "Group B", value: 4567 },
+  { name: "Group C", value: 1398 },
+  { name: "Group D", value: 9800 },
+  { name: "Group E", value: 3908 },
+  { name: "Group F", value: 4800 },
+];
 
 const Piecharts = () => {
   const data = [
@@ -37,6 +55,7 @@ const Piecharts = () => {
       </text>
     );
   };
+
   return (
     <>
       <div>
@@ -63,6 +82,30 @@ const Piecharts = () => {
             </ResponsiveContainer>
           </div>
         </div>
+      </div>
+      <div>
+        <PieChart width={1000} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data01}
+            cx={200}
+            cy={200}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Pie
+            dataKey="value"
+            data={data02}
+            cx={500}
+            cy={200}
+            innerRadius={40}
+            outerRadius={80}
+            fill="#82ca9d"
+          />
+          <Tooltip />
+        </PieChart>
       </div>
     </>
   );
